91b107db175edeb7dddabc41f6de71bfa5a59b22
marco <marco.paternosto@outlook.it>
2021-07-05 13:46:56 +0200

    added profile pic

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/color', (req, res) => {
     res.sendFile(path.join(__dirname, '/color.js'))
 })
 
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname, '/photo/profilo.jpg'))
+})
+
 app.listen(process.env.PORT || 3030, (req, res) => {
     console.log('listen on 3030')
 })
